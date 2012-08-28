950d5d1ff3ac1bece9c1ccc0625e13b81fff097e
Saroj <saroj.slg@gmail.com>
2012-08-28 03:46:45 -0700

    + Initial project checkin.

---
+++ SK_KVOExample/SK_KVOExample/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SK_KVOExample
+//
+//  Created by Saroj Sharma on 28/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SK_KVOExample/SK_KVOExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SK_KVOExample
+//
+//  Created by Saroj Sharma on 28/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
